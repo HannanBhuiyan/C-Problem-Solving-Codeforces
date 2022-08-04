528e8e239a15a3ef21d1728553983799bb75dc46
HannanBhuiyan <hannan.diuh@gmail.com>
2022-08-04 11:48:38 -0700

    Calculating Function

---
+++ PROBLEM/Calculating-Function.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long long n, b;
+    cin >> n;
+
+    if(n % 2 == 0) {
+        b = n / 2;
+    }else {
+        b = ( (n+1) / 2 ) * (-1);
+    }
+    cout << b << endl;
+return 0;
+}
